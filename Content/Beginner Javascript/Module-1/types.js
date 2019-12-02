9d11c4b9e1aff3b8dd9136d62f3e3b3f46907bab
Athoug Alsoughayer <athoug2.s@gmail.com>
2019-12-02 08:53:18 +0300

    type numbers

--- Content/Beginner Javascript/Module-1/types.js
+++ Content/Beginner Javascript/Module-1/types.js
@@ -94,3 +94,78 @@ const html = `
   </div>
 `;
 document.write(html);
+
+
+// --------------------------
+//          Number
+// --------------------------
+// There is only one type of number in javascript which in number
+// there is a handy keyword that can help us identify the type of variable
+// teh `typeof` lets test it out
+const age = 100;
+console.log(typeof age); // output -> number
+console.log(typeof name); // output -> string
+
+// we can create an integer [a whole number eg 100] or a float [which is a number that has a decimal eg 101.1]
+const integer = 42;
+const float = 42.5;
+
+// The operations available for us are:
+// addition +
+console.log(integer + float);
+// subtraction -
+console.log(integer - float);
+// multiplication *
+console.log(integer * float);
+// division /
+console.log(integer / float);
+//remainder / modules %
+console.log(integer % float);
+// and the power
+console.log(2 ** 3);
+
+// the only thing to keep in mind is when doing these operations on different types
+// example adding a number with a string the outcome won't be as suspected because
+// js does automatic type coercion (change type from one to another)
+console.log(1 + '1'); // output -> 11
+
+// along with numbers, we have helper methods
+console.log(Math.round(34.7));
+console.log(Math.floor(34.7));
+console.log(Math.ceil(34.7));
+console.log(Math.random()); // will give you a random number between 0 and 1
+
+// sometimes when adding floats in js it doesn't give you an accurate result
+console.log(0.1 + 0.2); // output -> 0.30000000000000004
+// this is a trick wes showed to show the reason behind the odd addition
+// window.location =`https://${0.1 + 0.2}.com`;
+
+// so when wanting to work with money, don't use decimals!!!
+// just convert them into cents
+const price = 10.34; // no no
+const rightPrice = 1034; // yes yes
+// this way we will always work with whole numbers
+// later on we will see how to work with currency values
+
+
+// Javascript also has infinity and - infinity
+// in the end of the day there is a max to what the computer can calculate
+// so if you try to calculate something too hight, it will return infinity or - infinity
+// example:
+console.log(1000 ** 2000); // output -> Infinity
+// yet they are numbers
+console.log(typeof (1000 ** 2000)); // output -> number
+
+// also you might encounter NaN which stand for not a number
+// say you divide a number by a string
+console.log(10 / 'dog'); // output -> not a number
+// it too is a type of number
+console.log(typeof NaN); // output -> number
+
+
+
+
+
+
+
+
