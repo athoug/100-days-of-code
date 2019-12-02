893a42c5fd0375f529997f391a740a131ad40109
Athoug Alsoughayer <athoug2.s@gmail.com>
2019-12-02 07:39:11 +0300

    types file

---
+++ Content/Beginner Javascript/Module-1/types.js
@@ -0,0 +1,12 @@
+const p = document.querySelector('p');
+
+p.textContent = 'Hey from the JS file';
+
+// TYpes can be remembered with teh acronym SNOB'N'US
+// s -> string
+// n -> number
+// o -> object (this is a special one because everything in JS is an object)
+// b -> boolean
+// n -> null
+// u -> undefined
+// s -> symbol
